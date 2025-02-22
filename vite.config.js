7a9c76b54222b8d985ed91bbdeb9db3c4ed68390
Shourya14sg <138760312+Shourya14sg@users.noreply.github.com>
2025-02-22 14:12:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  build: {
-    chunkSizeWarningLimit: 600, // Adjust to avoid warnings (default is 500)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@mui")) return "mui-vendor"; // Separate MUI into its own chunk
-            if (id.includes("react")) return "react-vendor"; // Separate React-related code
-            if (id.includes("recharts")) return "recharts-vendor"; // Separate Recharts for graphing
-            return "vendor"; // General vendor chunk
-          }
-        },
-      },
-    },
-  },
 })
