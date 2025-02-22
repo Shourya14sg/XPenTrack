725f21d7387f7ef30cd6f450a717548dcc31c4eb
Shourya14sg <shouryag064@gmail.com>
2025-02-22 12:16:07 +0530

    deployment

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,4 +1,5 @@
-export const domain="http://127.0.0.1:8000"
+export const domain=import.meta.env.VITE_API_URL;//"http://127.0.0.1:8000"
+
 export const expenseCategories = ["Food", "Transport", "Entertainment", "Utilities", "Healthcare", "Shopping", "Education", "Others"];
 export const Appname="SplitUp"
 
